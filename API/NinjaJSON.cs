1f64d5e728b3d8c1f9dfebfc8005819598d48fea
Frank D Kuenneke Home <theonlypax@gmail.com>
2021-01-19 09:39:55 -0500

    init code commit with template clas

---
+++ API/NinjaJSON.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace Cadiro
+{
+    public class NinjaJSON
+    {
+        private static string baseUrl = @"https://poe.ninja/api/data/";
+        private static string leagueUrl = @"?league=";
+
+
+
+        public static async Task<string> GetJSON()
+        {
+            //this would be item here
+            //string fullUrl = baseUrl + "GetDivinationCardsOverview" + leagueUrl + "Ritual";
+            string fullUrl = @"https://poe.ninja/api/data/itemoverview?league=Ritual&type=DivinationCard";
+        var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(fullUrl);
+            string result = await response.Content.ReadAsStringAsync();
+        return result;
+        }
+
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
-
+using Cadiro.API;
+using Newtonsoft.Json;
 namespace Cadiro
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             Root divinationCard = JsonConvert.DeserializeObject<Root>(NinjaJSON.GetJSON().Result);
+            //Console.WriteLine(NinjaJSON.GetJSON().Result.ToString());
+            for (int i = 0; i < divinationCard.lines.Count; i++)
+                Console.WriteLine(divinationCard.lines[i].name + " costs " + divinationCard.lines[i].chaosValue.ToString() + "c.");
         }
     }
 }
